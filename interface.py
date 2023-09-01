95f79f4eda4d0e44dad588131c8bdc38d8e427aa
Leoozito <leonardonogueira616@gmail.com>
2023-09-01 16:20:22 -0300

    Finalizado

--- interface.py
+++ interface.py
@@ -13,33 +13,33 @@ def on_leave(event):
     event.widget.configure(background="yellow")  # Restaura a cor de fundo original (branca) quando o mouse sai do botão
 
 def executar_arquivo():
-    subprocess.Popen(["python", "main.py"])
+    subprocess.Popen(["python", "main2.py"])
 
-# Especifique a cor de fundo original explicitamente como "white"
-btn1 = Button(app, text="Total 1° Pagamento", background="yellow", foreground="#000")
-btn1.place(x=220, y=20, width=150, height=30)
-btn1.bind("<Enter>", on_enter)
-btn1.bind("<Leave>", on_leave)
+# # Especifique a cor de fundo original explicitamente como "white"
+# btn1 = Button(app, text="Total 1° Pagamento", background="yellow", foreground="#000")
+# btn1.place(x=220, y=20, width=150, height=30)
+# btn1.bind("<Enter>", on_enter)
+# btn1.bind("<Leave>", on_leave)
 
-btn2 = Button(app, text="Total 2° Pagamento", background="yellow", foreground="#000")
-btn2.place(x=220, y=70, width=150, height=30)
-btn2.bind("<Enter>", on_enter)
-btn2.bind("<Leave>", on_leave)
+# btn2 = Button(app, text="Total 2° Pagamento", background="yellow", foreground="#000")
+# btn2.place(x=220, y=70, width=150, height=30)
+# btn2.bind("<Enter>", on_enter)
+# btn2.bind("<Leave>", on_leave)
 
-btn3 = Button(app, text="Total 3° Pagamento", background="yellow", foreground="#000")
-btn3.place(x=220, y=120, width=150, height=30)
-btn3.bind("<Enter>", on_enter)
-btn3.bind("<Leave>", on_leave)
+# btn3 = Button(app, text="Total 3° Pagamento", background="yellow", foreground="#000")
+# btn3.place(x=220, y=120, width=150, height=30)
+# btn3.bind("<Enter>", on_enter)
+# btn3.bind("<Leave>", on_leave)
 
-btn4 = Button(app, text="Total 4° Pagamento", background="yellow", foreground="#000")
-btn4.place(x=220, y=170, width=150, height=30)
-btn4.bind("<Enter>", on_enter)
-btn4.bind("<Leave>", on_leave)
+# btn4 = Button(app, text="Total 4° Pagamento", background="yellow", foreground="#000")
+# btn4.place(x=220, y=170, width=150, height=30)
+# btn4.bind("<Enter>", on_enter)
+# btn4.bind("<Leave>", on_leave)
 
-btn5 = Button(app, text="Total 5° Pagamento", background="yellow", foreground="#000")
-btn5.place(x=220, y=220, width=150, height=30)
-btn5.bind("<Enter>", on_enter)
-btn5.bind("<Leave>", on_leave)
+# btn5 = Button(app, text="Total 5° Pagamento", background="yellow", foreground="#000")
+# btn5.place(x=220, y=220, width=150, height=30)
+# btn5.bind("<Enter>", on_enter)
+# btn5.bind("<Leave>", on_leave)
 
 btn6 = Button(app, text="Total dos Pagamentos Mensais", background="white", foreground="#000", command=executar_arquivo)
 btn6.place(x=180, y=270, width=240, height=50)
--- main2.py
+++ main2.py
@@ -2,8 +2,8 @@ import openpyxl
 import os
 import pyautogui
 import subprocess
-import threading
-import time  # Importe o módulo time
+import pyperclip
+import time
 
 # Pegar dados do Excel
 arquivo_excel = openpyxl.load_workbook("arquivo_excel.xlsx")
@@ -12,54 +12,53 @@ dados = arquivo_excel.active
 total_linhas = dados.max_row
 total_colunas = dados.max_column
 
-# Caminho onde será salvo o arquivo (Área de Trabalho)
+# Caminho onde será salvo o arquivo
 desktop_path = os.path.expanduser("~/Documents")
 nome_arquivo = "relatorio.txt"
 file_path = os.path.join(desktop_path, nome_arquivo)
 
 lista_valores = []  # Lista que será randomizada na calculadora
 
-def open_notepad():
-    os.system(f"notepad.exe {file_path}")
-
-def open_calculator():
-    calculator_process = subprocess.Popen(["calc.exe"])
-    calculator_title = "Calculadora"
-
-    time.sleep(5)  # Aguarde um tempo maior (5 segundos) para a janela da calculadora abrir
-
-    calculator_windows = pyautogui.getWindowsWithTitle(calculator_title)
-
-    if calculator_windows:  # Verifique se a lista não está vazia
-        calculator_window = calculator_windows[0]
-
-        calculator_window.activate()
-
-        for valor in lista_valores:
-            pyautogui.write(str(valor))
-            pyautogui.press('enter')
-
-        calculator_process.wait()
-
 with open(file_path, "w") as file:
     for linha in range(1, total_linhas + 1):  # para manipular cada linha
         for coluna in range(1, total_colunas + 1):  # para manipular cada coluna
             celula = dados.cell(row=linha, column=coluna)
             valor = celula.value
+
             if valor is not None:
                 conteudo = f"linha {linha}, coluna {coluna}: {valor}\n"
-                if isinstance(valor, int):
+                if type(valor) == int:
                     lista_valores.append(valor)
                 file.write(conteudo)  # adiciona as informações do Excel no arquivo
 
-# Criação das threads
-notepad_thread = threading.Thread(target=open_notepad)
-calculator_thread = threading.Thread(target=open_calculator)
+os.system(f"start {file_path}")
+time.sleep(2)
 
-# Inicia as threads
-notepad_thread.start()
-calculator_thread.start()
+# Abre a calculadora do Windows
+subprocess.Popen(["calc.exe"])
+time.sleep(2.5)
 
-# Aguarda as threads terminarem
-notepad_thread.join()
-calculator_thread.join()
+# Loop para inserir valores e calcular
+for i, valor in enumerate(lista_valores):
+    pyautogui.write(str(valor))
+    if i < len(lista_valores) - 1:
+        pyautogui.press('enter')
+        pyautogui.press('+')
+
+# Pressiona Enter para obter o resultado final
+pyautogui.press('enter')
+
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(2)
+resultado = pyperclip.paste()
+time.sleep(1)
+with open(file_path, 'a') as file:  # Use 'a' para modo de anexação
+    file.write(resultado)
+
+célula_resultado = dados.cell(row=23, column=2)  # Por exemplo, adicione à primeira linha da última coluna
+célula_resultado.value = resultado
+time.sleep(2)
+# Salve as alterações no arquivo Excel
+arquivo_excel.save("arquivo_excel.xlsx")
+pyautogui.hotkey('f5')
+os.system("start \"\" \"C:\\Program Files\\LibreOffice\\program\\soffice.exe\" --calc arquivo_excel.xlsx")
