b1400440ed025ee3e0273956d68e839d788d276a
Leoozito <leonardonogueira616@gmail.com>
2023-09-03 22:52:53 -0300

    Ajustes feito no codigo

--- main.py
+++ main.py
@@ -1,49 +1,64 @@
-from tkinter import *
+import openpyxl
+import os
+import pyautogui
 import subprocess
+import pyperclip
+import time
 
-app = Tk()
-app.title("Total dos Pagamentos Mensais")
-app.geometry("600x320")
-app.configure(background="#008")
+# Pegar dados do Excel
+arquivo_excel = openpyxl.load_workbook("arquivo_excel.xlsx")
+dados = arquivo_excel.active
 
-def on_enter(event):
-    event.widget.configure(background="white")  # Muda a cor de fundo para amarelo quando o mouse entra no botão
+total_linhas = dados.max_row
+total_colunas = dados.max_column
 
-def on_leave(event):
-    event.widget.configure(background="yellow")  # Restaura a cor de fundo original (branca) quando o mouse sai do botão
+# Caminho onde será salvo o arquivo
+desktop_path = os.path.expanduser("~/Documents")
+nome_arquivo = "relatorio.txt"
+file_path = os.path.join(desktop_path, nome_arquivo)
 
-def executar_arquivo():
-    subprocess.Popen(["python", "system_wind.py"])
+lista_valores = []  # Lista que será randomizada na calculadora
 
-# # Especifique a cor de fundo original explicitamente como "white"
-# btn1 = Button(app, text="Total 1° Pagamento", background="yellow", foreground="#000")
-# btn1.place(x=220, y=20, width=150, height=30)
-# btn1.bind("<Enter>", on_enter)
-# btn1.bind("<Leave>", on_leave)
+with open(file_path, "w") as file:
+    for linha in range(1, total_linhas + 1):  # para manipular cada linha
+        for coluna in range(1, total_colunas + 1):  # para manipular cada coluna
+            celula = dados.cell(row=linha, column=coluna)
+            valor = celula.value
 
-# btn2 = Button(app, text="Total 2° Pagamento", background="yellow", foreground="#000")
-# btn2.place(x=220, y=70, width=150, height=30)
-# btn2.bind("<Enter>", on_enter)
-# btn2.bind("<Leave>", on_leave)
+            if valor is not None:
+                conteudo = f"linha {linha}, coluna {coluna}: {valor}\n"
+                if type(valor) == int:
+                    lista_valores.append(valor)
+                file.write(conteudo)  # adiciona as informações do Excel no arquivo
 
-# btn3 = Button(app, text="Total 3° Pagamento", background="yellow", foreground="#000")
-# btn3.place(x=220, y=120, width=150, height=30)
-# btn3.bind("<Enter>", on_enter)
-# btn3.bind("<Leave>", on_leave)
+os.system(f"start {file_path}")
+time.sleep(2)
 
-# btn4 = Button(app, text="Total 4° Pagamento", background="yellow", foreground="#000")
-# btn4.place(x=220, y=170, width=150, height=30)
-# btn4.bind("<Enter>", on_enter)
-# btn4.bind("<Leave>", on_leave)
+# Abre a calculadora do Windows
+subprocess.Popen(["calc.exe"])
+time.sleep(3)
 
-# btn5 = Button(app, text="Total 5° Pagamento", background="yellow", foreground="#000")
-# btn5.place(x=220, y=220, width=150, height=30)
-# btn5.bind("<Enter>", on_enter)
-# btn5.bind("<Leave>", on_leave)
+# Loop para inserir valores e calcular
+for i, valor in enumerate(lista_valores):
+    pyautogui.write(str(valor))
+    if i < len(lista_valores) - 1:
+        pyautogui.press('enter')
+        pyautogui.press('+')
 
-btn6 = Button(app, text="Total dos Pagamentos Mensais", background="white", foreground="#000", command=executar_arquivo)
-btn6.place(x=180, y=270, width=240, height=50)
-btn6.bind("<Enter>", on_enter)
-btn6.bind("<Leave>", on_leave)
+# Pressiona Enter para obter o resultado final
+pyautogui.press('enter')
 
-app.mainloop()
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(2)
+resultado = pyperclip.paste()
+time.sleep(1)
+with open(file_path, 'a') as file:  # Use 'a' para modo de anexação
+    file.write(resultado)
+
+célula_resultado = dados.cell(row=23, column=2)  # Por exemplo, adicione à primeira linha da última coluna
+célula_resultado.value = resultado
+time.sleep(2)
+# Salve as alterações no arquivo Excel
+arquivo_excel.save("arquivo_excel.xlsx")
+pyautogui.hotkey('f5')
+os.system("start \"\" \"C:\\Program Files\\LibreOffice\\program\\soffice.exe\" --calc arquivo_excel.xlsx")
